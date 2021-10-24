379f710760d0fc258232b99dcb8ccedf156a2673
Team Haztech <teamhaztech@gmail.com>
2021-10-24 13:25:44 +0530

    added resume viewer

--- script.js
+++ script.js
@@ -27,25 +27,38 @@ $(document).ready(function () {
     repositionLabel();
 });
 
-
-function buildComponents(){
-
-}
-
-
-
 //Selectors
 const menuButton = $("#menu-btn")
 const menuButtonClose = $("#menu-btn-close")
 const menuView = $("#ms-menu")
+const resumeView = $("#resume-viewer")
+const resumeViewBtn = $("#resume-view-btn")
+const resumeViewBtnMenu = $("#resume-view-btn-menu")
+const resumeViewBtnClose = $("#resume-view-btn-close")
 const scrollPercentElement = $("#scroll-percentage")
 //Events
+
+
 menuButton.click(openMenu);
 menuButtonClose.click(closeMenu)
+resumeViewBtn.click(showResume)
+resumeViewBtnMenu.click(showResume)
+resumeViewBtnClose.click(closeResumeViewer)
 
+
+
+
+
+function initState(){
+    menuView.hide()
+    resumeView.hide()
+}
+
+initState();
 
 //Function
 function openMenu() {
+    menuView.show()
     menuButton.hide()
     $(menuView).css({
         'transform': 'translateX(0%)scale(1)',
@@ -64,4 +77,31 @@ function closeMenu() {
         'overflow-x': 'hidden',
         'overflow-y': 'scroll',
     });
+    setTimeout(function(){
+        menuView.hide()
+    }, 1000)
+}
+
+function showResume(){
+    resumeView.show()
+    $(resumeView).css({
+        'transform': 'translateX(0%)scale(1)',
+        'border-radius': '0%'
+    });
+    $('body').css('overflow', 'hidden');
+}
+
+
+function closeResumeViewer(){
+    $(resumeView).css({
+        'transform': 'translateX(-100%)scale(0.1)',
+        'border-radius': '20%'
+    });
+    $('body').css({
+        'overflow-x': 'hidden',
+        'overflow-y': 'scroll',
+    });
+    setTimeout(function(){
+        resumeView.hide()
+    }, 1000)
 }
