20f8a7858be7f882569f6d4c7a1380525a6b41fa
Team Haztech <teamhaztech@gmail.com>
2021-10-24 11:20:33 +0530

    added content for services

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
         const scrollPercentElement = document.getElementById("scroll-percentage")
         scrollPercentElement.style = "width:" + scrollPercentRounded + "%;z-index:2"
         repositionLabel();
-        console.log(scrollPercentRounded);
     });
 
     $(window).resize(function () {
@@ -65,34 +64,3 @@ function closeMenu() {
         'overflow-y': 'scroll',
     });
 }
-
-
-// function loadServicesAnimations() {
-//     const appDevAnimationElement = document.getElementById("app_dev_animation")
-//     const webDevAnimationElement = document.getElementById("web_dev_animation")
-//     const seoAnimationElement = document.getElementById("seo_animation")
-
-//     bodymovin.loadAnimation({
-//         container: appDevAnimationElement,
-//         renderer: 'svg',
-//         loop: true,
-//         autoplay: true,
-//         path: 'assets/lottie-animations/app-dev.json' // the path to the animation json
-//     })
-//     bodymovin.loadAnimation({
-//         container: webDevAnimationElement,
-//         renderer: 'svg',
-//         loop: true,
-//         autoplay: true,
-//         path: 'assets/lottie-animations/web-development-design.json' // the path to the animation json
-//     })
-//     bodymovin.loadAnimation({
-//         container: seoAnimationElement,
-//         renderer: 'svg',
-//         loop: true,
-//         autoplay: true,
-//         path: 'assets/lottie-animations/seo.json' // the path to the animation json
-//     })
-// }
-
-
